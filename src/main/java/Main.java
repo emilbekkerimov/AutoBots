589e963abe0da7bceee5c072de87ebdd00305fb2
emilbekkerimov <emkerimov@mail.ru>
2025-07-08 23:54:44 +0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,5 @@ public class Main {
     // Hello world
     // В один день я посмотрю это сообщение и буду думать да это было начало
     // теперь проверим git pull
+    // git pull vs git fetch
 }
